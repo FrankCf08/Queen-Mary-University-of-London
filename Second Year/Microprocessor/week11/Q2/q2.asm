a3e465f7ae2af8a6568830b863765a83896ed0c2
Frank Cf <frankcruz_08@hotmail.com>
2019-09-02 18:19:42 +0100

    week11

---
+++ Second Year/Microprocessor/week11/Q2/q2.asm
@@ -0,0 +1,25 @@
+ORG 000H
+
+start:
+	JB P3.2, b2
+	MOV A, #'Q'
+	SJMP out
+b2:
+	JB P3.3, b3
+ 	MOV A, #'M'
+ 	SJMP out
+b3:
+ 	JB P3.4, b4
+  	MOV A, #'U'
+  	SJMP out
+b4:
+  	JB P3.5, start
+  	MOV A, #'L'
+
+out:
+  	LCALL send
+  	SJMP start
+
+send:
+  	MOV P1,A
+  	RET
